d5783c82668612f4aec377e031595b6d65fe38cf
Luis C. Ramalho <ramalho.luiscarlos@gmail.com>
2023-01-22 20:31:34 -0300

    atualização

--- main.js
+++ main.js
@@ -2,6 +2,8 @@ const inputTask = document.querySelector('#inputTask')
 const btnAddTask = document.querySelector('.btnSubmit')
 const taskAdded = document.querySelector('.taskAdded')
 
+
+
 const validateInput = ()=> inputTask.value.trim().length > 0//forma reduzida do if
 
 function addTask(){
@@ -28,6 +30,8 @@ function addTask(){
 
     inputTask.value =''
 
+    updateLocalSorage() 
+
     newElementThrash.addEventListener('click', () => itemDeleteTask(taskAdded, newParagraph))
         
 }
@@ -40,6 +44,7 @@ const itemDeleteTask = (taskAdded, newParagraph) => {
             task.remove()
         }
     }
+    updateLocalSorage()
 }
 
 
@@ -51,6 +56,7 @@ const completedTask = (newParagraph) => {
             task.firstChild.classList.toggle('taskSelected')
         }
     }
+    updateLocalSorage()
 }
 
 //Verificando se o input está vazio
@@ -61,5 +67,54 @@ const inputChange = ()=>{
     }
 }
 
+//Salvando as tasks no LocalStorage, atualizando e mostrando na tela
+const updateLocalSorage = () => {
+    const tasks = taskAdded.childNodes
+
+    const localStorageTasks = [...tasks].map((task) =>{
+        const content = task.firstChild
+        const isCompleted = content.classList.contains('taskSelected')
+
+        return {description: content.innerText, isCompleted}
+    })
+
+    console.log(localStorageTasks)
+
+    localStorage.setItem('tasks', JSON.stringify(localStorageTasks))
+}
+
+
+//carregando do LocalStorage as tasks salvas
+const taskinLocalStorage = () =>{
+    const taskFromLocalStorage = JSON.parse(localStorage.getItem('tasks'))
+    
+    if(!taskFromLocalStorage) return // corrigindo erro quando o app inicia sem task salva no LocalStorage
+    
+    for (const tasks of taskFromLocalStorage) {
+        const newTaskContainer = document.createElement('div')
+        newTaskContainer.classList.add('createdDiv')
+        taskAdded.appendChild(newTaskContainer)
+
+        const newParagraph = document.createElement('p')
+        newParagraph.innerText = tasks.description
+
+        if(tasks.isCompleted){
+            newParagraph.classList.add('taskSelected')
+        }
+        
+        newTaskContainer.appendChild(newParagraph)
+        newParagraph.addEventListener('click', () => completedTask(newParagraph))
+
+        //criando o icon de exlusão
+        let newElementThrash = document.createElement('i')
+        newElementThrash.setAttribute('class', 'fa-solid fa-trash-can')
+        newTaskContainer.appendChild(newElementThrash)
+
+        newElementThrash.addEventListener('click', () => itemDeleteTask(taskAdded, newParagraph))
+    }
+}
+
+
+taskinLocalStorage()
 btnAddTask.addEventListener('click',addTask)
 inputTask.addEventListener('change', inputChange)
